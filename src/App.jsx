dcc4e28762dab8f708e2af37c8034f7c20d7d27d
YuriiMat <yurii.matviichuk@sigma.software>
2025-12-26 00:01:16 +0200

    Task 1. Create a simple Task component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <h1>Movies React App</h1>;
-}
+import { useState } from 'react';
+import Task from './components/Task';
 
-export default App;
+export default function App() {
+  const [isTaskCompleted, setIsTaskCompleted] = useState(false);
+  const handleStatusChange = () => setIsTaskCompleted((prev) => !prev);
+
+  return (
+    <div className='m-3 flex flex-col items-center gap-2'>
+      <Task
+        title='To learn React and enjoy it'
+        description='Learning React is fun and exciting! It gives a completely new perspective on building web applications.'
+        isCompleted={isTaskCompleted}
+        changeStatus={handleStatusChange}
+      />
+    </div>
+  );
+}
---
+++ src/components/Task.jsx
@@ -0,0 +1,30 @@
+export default function Task({
+  changeStatus,
+  description,
+  isCompleted,
+  title,
+}) {
+  return (
+    <div
+      className={`w-84 border-2 rounded-md flex flex-col gap-3 p-4 text-lg ${
+        isCompleted ? 'border-green-500' : 'border-indigo-600'
+      }`}
+    >
+      <h2 className='bg-lime-200 p-2 rounded-md font-mono text-justify'>
+        <span className='font-bold'>Title:</span> {title}
+      </h2>
+      <p className='bg-cyan-300 p-2 rounded-md font-mono text-justify'>
+        <span className='font-bold'>Description: </span>
+        {description}
+      </p>
+      <button
+        className={`px-4 py-2 text-white rounded-md cursor-pointer ${
+          isCompleted ? 'bg-green-500' : 'bg-indigo-600'
+        }`}
+        onClick={changeStatus}
+      >
+        {isCompleted ? 'Undo' : 'Mark as completed'}
+      </button>
+    </div>
+  );
+}
